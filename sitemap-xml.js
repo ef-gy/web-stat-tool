41d9556de233b8e297e67b81b4fc7ea683643d55
Magnus Deininger <magnus@ef.gy>
2014-08-27 01:35:23 +0400

    add XML output variant for the sitemap processor

---
+++ sitemap-xml.js
@@ -0,0 +1,37 @@
+var webstats = require('./lib/webstats');
+
+var _urls = {};
+var _domains = {};
+
+webstats.getSitemapAggregate(process.argv.slice(2), function(urls, domains) {
+  _urls = urls;
+  _domains = domains;
+});
+
+process.on('exit', function(code) {
+  var out = '<urls>';
+
+  for (i in _urls) {
+    if (_urls[i].total > 0) {
+      var e = _urls[i];
+      out += '<url id="' + i +
+             '" total="' + e.total +
+             '" twitter="' + e.twitter +
+             '" facebook="' + e.facebook +
+             '" google-plus="' + e.googleplus +
+             '" reddit="' + e.reddit +
+             '" stumbleupon="' + e.stumbleupon +
+             '" linkedin="' + e.linkedin
+           + '"/>';
+    }
+  }
+
+  for (i in _domains)
+  {
+    out += '<domain id="' + i + '" alexa-rank="' + _domains[i].alexaRank + '"/>';
+  }
+
+  out += '</urls>';
+
+  console.log(out);
+});
